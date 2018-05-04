d94fe67ce9ca12c5d1558b81ec7ccf41fb05f342
pair <pair@digithun.co.th>
2018-05-04 15:11:19 +0700

    first commit

---
+++ pages/second.js
@@ -0,0 +1,10 @@
+import Layout from '../components/MyLayout.js'
+
+export default () => (
+  <Layout>
+    <h1>Second</h1>
+    <p>
+      This is the second page. Fix Hide Chatbox
+    </p>
+  </Layout>
+)
